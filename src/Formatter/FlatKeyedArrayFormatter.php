dd1d58013d6bf3c339ae2c0719b6707db5ec80db
Nat Zimmermann <nat@ntzm.io>
2019-03-08 22:05:19 +0000

    add FlatArrayFormatter and rename

--- src/Formatter/FlatKeyedArrayFormatter.php
+++ src/Formatter/FlatKeyedArrayFormatter.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace Ntzm\Isok\Formatter;
 
 use Ntzm\Isok\Violation\Violations;
+use function implode;
+use function iterator_to_array;
 
-final class FlatArrayFormatter
+final class FlatKeyedArrayFormatter
 {
-    /** @return string[] */
+    /** @return array<string, array<int, string>> */
     public function format(Violations $violations) : array
     {
         $result = [];
 
         foreach ($violations as $violation) {
-            $result[] = $violation->reason();
+            $key = implode('.', iterator_to_array($violation->path()));
+
+            $result[$key][] = $violation->reason();
         }
 
         return $result;
